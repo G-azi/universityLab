4c118171e4daa323087e35c36c59fb299a628754
Gazi Hossan <jahid_reco@yahoo.com>
2015-05-05 17:34:31 +0100

    add all the file

---
+++ maths.c
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+int main() 
+{
+
+int a=20;
+int b=10;
+int result;
+printf("addition of a and b= %d\n", result=a+b);
+printf("substraction of a and b= %d\n", result=a-b);
+printf("multiply of a and b=%d\n", result=a*b);
+printf("devide of a and b=%d\n", result=a/b);
+
+}
